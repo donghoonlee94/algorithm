debff18ed370ce58333873a7f49fbfa0460b5319
donghoonlee <94donghoon.lee@gmail.com>
2024-10-14 21:44:03 +0900

    시간 재기

--- big_o_notation_1.js
+++ big_o_notation_1.js
@@ -1,10 +1,27 @@
-function addUpto(n) {
+function addUpToA(n) {
   let total = 0;
   for (let i = 1; i <= n; i++) {
-    total += 1;
+    total += i;
   }
   return total;
 }
 
 // 6
-console.log(addUpto(6));
+console.log(addUpToA(6));
+
+function addUpToB(n) {
+  return (n * (n + 1)) / 2;
+}
+
+// 6
+console.log(addUpToB(6));
+
+const t0 = performance.now();
+addUpToA(100000000);
+const t1 = performance.now();
+console.log(`Call to doSomething took ${(t1 - t0) / 1000} seconds.`);
+
+const t2 = performance.now();
+addUpToB(100000000);
+const t3 = performance.now();
+console.log(`Call to doSomething took ${(t3 - t2) / 1000} seconds.`);

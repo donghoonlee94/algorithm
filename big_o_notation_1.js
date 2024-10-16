e4e6ac0aa14a76231c11748663dc0cebe4f240a3
donghoonlee <94donghoon.lee@gmail.com>
2024-10-16 15:32:44 +0900

    Add annotation

--- big_o_notation_1.js
+++ big_o_notation_1.js
@@ -36,3 +36,12 @@ function printAllParis(n) {
     }
   }
 }
+
+// O(1) - 상수 시간 복잡도 (Constant Time Complexity)
+// 항상 일정한 시간이 걸리는 알고리즘.
+// O(n) - 선형 시간 복잡도 (Linear Time Complexity)
+// 입력 크기 n에 비례하여 시간이 증가하는 알고리즘.
+// O(n²) - 이차 시간 복잡도 (Quadratic Time Complexity)
+// 입력 크기 n이 증가할 때, 실행 시간이 n²에 비례하여 증가하는 알고리즘.
+// O(log n) - 로그 시간 복잡도 (Logarithmic Time Complexity)
+// O(n log n) - 선형 로그 시간 복잡도 (Linearithmic Time Complexity)

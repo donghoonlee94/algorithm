5cb1cccbeb93ca0718b91c55b711d99cbd77584d
donghoonlee <94donghoon.lee@gmail.com>
2024-10-23 18:36:21 +0900

    Hash table_2

--- hash_tables.js
+++ hash_tables.js
@@ -1,14 +1,26 @@
 // Only string & O(n)
+// function hash(key, arrayLen) {
+//   let total = 0;
+//   for (let char of key) {
+//     let value = char.charCodeAt(0) - 96;
+//     total = (total + value) % arrayLen;
+//   }
+
+//   return total;
+// }
+
+// hash("pink", 10);
+// hash("orange", 10);
+// hash("green", 10);
+
 function hash(key, arrayLen) {
   let total = 0;
-  for (let char of key) {
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
     let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+    total = (total * WEIRD_PRIME + value) & arrayLen;
   }
 
   return total;
 }
-
-hash("pink", 10);
-hash("orange", 10);
-hash("green", 10);

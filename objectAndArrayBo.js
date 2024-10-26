0bc776c35d6d97ca04795006ba3feacd97fce241
donghoonlee <94donghoon.lee@gmail.com>
2024-10-26 16:15:06 +0900

    Object & Array Big O

---
+++ objectAndArrayBo.js
@@ -0,0 +1,17 @@
+// Object
+// Insertion -> O(1)
+// Removal -> (O(1)
+// Access -> O(1)
+// Searching -> O[n]
+// keys, values, entires -> O(n)
+// hasOwnProperty -> O(1)
+
+// Array
+// Insertion -> It depends
+// Removal -> It depends
+// Searching (O)n
+// Access O(1)
+// push, pop -> O(1)
+// shift, unshift, concat, slice, splice -> O(n)
+// sort -> O(N * log N)
+// forEach, map, filter, reduce... -> O(n)

e6c250bad107789ce4c836f2f889a2adfd14210a
donghoonlee <94donghoon.lee@gmail.com>
2024-10-27 22:16:39 +0900

    Doubly linked lists > push

--- doublyLinkedLists.ts
+++ doublyLinkedLists.ts
@@ -1,14 +1,14 @@
 interface INode {
-  val: string | number;
-  next: INode;
-  prev: INode;
+  val: string | number | null;
+  next: INode | null;
+  prev: INode | null;
 }
 
 type Nullable<T> = {
   [K in keyof T]: T[K] | null;
 };
 
-class ListNode implements Nullable<INode> {
+class ListNode implements INode {
   val: string | number | null;
   next: INode | null;
   prev: INode | null;
@@ -20,7 +20,14 @@ class ListNode implements Nullable<INode> {
   }
 }
 
-class DoublyLinkedList {
+interface DoublyLinkedList {
+  head: ListNode | null;
+  tail: ListNode | null;
+  length: number;
+  push(val: string | number): DoublyLinkedList;
+}
+
+class DoublyLinkedList implements DoublyLinkedList {
   head: ListNode | null;
   tail: ListNode | null;
   length: number;
@@ -30,7 +37,29 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val: string | number): DoublyLinkedList {
+    const newNode = new ListNode(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail!.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
 const List = new DoublyLinkedList();
+const node1 = new ListNode(12);
+node1.next = new ListNode(11);
+
+List.push(99);
+List.push(100);
+
 console.log(List);
+console.log(node1);

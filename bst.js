794bea3950cfd0c4b0bd6eb7bf787bba40e951ba
donghoonlee <94donghoon.lee@gmail.com>
2024-11-06 16:44:33 +0900

    Binary Search Tree > class

--- bst.js
+++ bst.js
@@ -1,3 +1,22 @@
 // Binary Search tree
 // 자식은 최대 2개.
 // 왼쪽은 낮은 값, 우측은 높은 값을 기준으로 정렬된 트리.
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(15);
